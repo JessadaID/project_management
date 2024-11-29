a6c67dbc6971a2824df95de0d3c2c2d7d286b4bd
Jessada <113906747+JessadaID@users.noreply.github.com>
2024-11-29 16:59:54 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import { Router, Route, Link } from "svelte-routing";
   import HomePage from "./HomePage.svelte";
   
-  import Cpe02_form from "./sub_cpe/cpe02_form.svelte";
   import Cpe02_wait from "./sub_cpe/cpe02_wait.svelte";
   import Cpe02_home from "./sub_cpe/cpe02_home.svelte";
 
@@ -19,7 +18,6 @@
     
     <Route path="/" component={HomePage} />
     <Route path="/cpe" component={Cpe02_home} />
-    <Route path="/cpe_form" component={Cpe02_form} />
     <Route path="/cpe_wait" component={Cpe02_wait} />
     
   </Router>
