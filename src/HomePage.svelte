22a30ddb3154a38e2ec9e5e222c574ec32b4efd8
Jessada <113906747+JessadaID@users.noreply.github.com>
2024-11-29 17:00:24 +0700

    Update HomePage.svelte

--- src/HomePage.svelte
+++ src/HomePage.svelte
@@ -12,26 +12,6 @@
         <b>ใบเสนอโครงงาน CPE02</b>
       </div>
     </Link>
-    <Link to="/cpe">
-      <div
-        class="hover:bg-red-200 p-5 border-gray-500 border-2 bg-white aspect-w-1 aspect-h-1 flex items-center justify-center"
-      >
-        <b>บทที่ 1</b>
-      </div>
-    </Link>
-    <Link to="/cpe">
-      <div
-        class="hover:bg-red-200 p-5 border-gray-500 border-2 bg-white aspect-w-1 aspect-h-1 flex items-center justify-center"
-      >
-        <b>บทที่ 2</b>
-      </div>
-    </Link>
-    <Link to="/cpe">
-      <div
-        class="hover:bg-red-200 p-5 border-gray-500 border-2 bg-white aspect-w-1 aspect-h-1 flex items-center justify-center"
-      >
-        <b>บทที่ 3</b>
-      </div>
-    </Link>
+   
   </div>
 </main>
