d87deea48b7f39c6254c482b19e49dd0014f7260
Jessada <113906747+JessadaID@users.noreply.github.com>
2024-11-29 09:16:37 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,7 @@
   import Cpe02_form from "./sub_cpe/cpe02_form.svelte";
   import Cpe02_wait from "./sub_cpe/cpe02_wait.svelte";
   import Cpe02_home from "./sub_cpe/cpe02_home.svelte";
-  import Cpe02_info from "./sub_cpe/cpe02_info.svelte";
-  import Cpe02_edit from "./sub_cpe/cpe02_edit.svelte";
+
 </script>
 
 <main class="">
@@ -22,7 +21,6 @@
     <Route path="/cpe" component={Cpe02_home} />
     <Route path="/cpe_form" component={Cpe02_form} />
     <Route path="/cpe_wait" component={Cpe02_wait} />
-    <Route path="/cpe_wait/:id" component={Cpe02_info} />
-    <Route path="/cpe_edit/:id" component={Cpe02_edit} />
+    
   </Router>
 </main>
