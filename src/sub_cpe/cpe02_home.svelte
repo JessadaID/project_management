8cee8e92776b5d39dbf104a7e8fe5ed48a0870f1
Jessada <113906747+JessadaID@users.noreply.github.com>
2024-11-29 16:59:35 +0700

    Update cpe02_home.svelte

--- src/sub_cpe/cpe02_home.svelte
+++ src/sub_cpe/cpe02_home.svelte
@@ -4,11 +4,7 @@
 
 <main>
   <div class="grid md:grid-cols-2 gap-4 m-5 rounded">
-    <Link to="/cpe_form">
-      <div class="hover:bg-red-200 p-5 border-gray-500 border-2 bg-white">
-        <b>กรอกแบบฟอร์ม CPE02</b>
-      </div>
-    </Link>
+    
   
     <Link to="/cpe_wait">
       <div class="hover:bg-red-200 p-5 border-gray-500 border-2 bg-white">
